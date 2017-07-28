5503bb5e542b988ada1a552046f6696f402c2e53
sushilnagpal <sushilnagpal20@gmail.com>
2017-07-28 14:23:14 -0700

    Added test case - Environment variable dependent.

--- api/leads/-id/convert/GET/__tests__/test.js
+++ api/leads/-id/convert/GET/__tests__/test.js
@@ -1,11 +1,19 @@
 import request from 'supertest';
-import assert from 'assert';
-import app from '../../../../';
-
-const server = app.createServer();
+import app from '../../../../../../';
 
 describe('TEST GET /leads/:id/convert', () => {
-    it('define a testcase here', (done) => {
-        done();
+    let server;
+
+    before(async() => {
+        server = await app.createServer();
+    });
+
+    it('should get 400 for /leads/1/convert', (done) => {
+        request(server)
+            .get('/api/leads/1/convert')
+            .expect(400, (err, res) => {
+                if (err) return done(err);
+                done();
+            });
     });
 });
--- api/leads/score/GET/__tests__/test.js
+++ api/leads/score/GET/__tests__/test.js
@@ -1,8 +1,6 @@
 import request from 'supertest';
 import assert from 'assert';
-import app from '../../../../';
 
-const server = app.createServer();
 
 describe('TEST GET /leads/score', () => {
     it('define a testcase here', (done) => {
